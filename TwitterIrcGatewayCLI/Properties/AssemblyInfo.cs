427a127ba4fb5ba98a5a3a0a9d9d52dddb82a5a1
tomoyo <tomoyo@07c13e24-2e35-da11-a9e7-00b0d02dc07a>
2009-03-20 14:33:38 +0000

    バージョン番号を変更した。
    git-svn-id: http://svn.misuzilla.org/etcetera/TwitterIrcGateway/trunk@466 07c13e24-2e35-da11-a9e7-00b0d02dc07a

--- TwitterIrcGatewayCLI/Properties/AssemblyInfo.cs
+++ TwitterIrcGatewayCLI/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("TwitterIrcGatewayCLI")]
-[assembly: AssemblyCopyright("Copyright (C) Microsoft 2007")]
+[assembly: AssemblyCopyright("Copyright (C) Misuzilla.org 2007")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
