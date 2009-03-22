2af534a73e879ce6b5edfef39f6ee523303ed516
tomoyo <tomoyo@07c13e24-2e35-da11-a9e7-00b0d02dc07a>
2009-03-22 09:41:38 +0000

    ちょっとサンプルを更新。
    git-svn-id: http://svn.misuzilla.org/etcetera/TwitterIrcGateway/trunk@481 07c13e24-2e35-da11-a9e7-00b0d02dc07a

--- ExtraAddIns/DLRIntegration/Samples/attach_event.rb
+++ ExtraAddIns/DLRIntegration/Samples/attach_event.rb
@@ -1,7 +1,23 @@
-include System
+# ステータス更新直前のイベント
+Session.pre_send_update_status do |sender, e|
+	# CLR String -> Ruby String に変換する必要があるっぽい
+	if e.text.to_s.include?("はうはう")
+		Session.send_server(Misuzilla::Net::Irc::NoticeMessage.new(e.received_message.receiver, "はうはう税を徴収する!それまでははうはうさせない!"))
 
-on_pre_send_message_timeline_status = Proc.new do |sender, e|
+		# キャンセルすることで送信させない
+		e.cancel = true
+	end
+end
+
+# タイムラインの一ステータスを受信してクライアントに送信する直前のイベント
+Session.pre_send_message_timeline_status do |sender, e|
 	e.text = "#{e.text} (by #{e.status.user.name})"
 end
 
-Session.pre_send_message_timeline_status(&on_pre_send_message_timeline_status)
+# IRCメッセージを受け取ったときのイベント
+Session.message_received do |sender, e|
+	if e.message.command.to_s == "HAUHAU"
+		Session.send_server_error_message("Hauhau!")
+		e.cancel = true
+	end
+end
