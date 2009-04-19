8b6f0133a9aa04f40f228a40900cdad5f9e18c90
tomoyo <tomoyo@07c13e24-2e35-da11-a9e7-00b0d02dc07a>
2009-04-19 19:44:23 +0000

    インデントを修正。
    git-svn-id: http://svn.misuzilla.org/etcetera/TwitterIrcGateway/trunk@530 07c13e24-2e35-da11-a9e7-00b0d02dc07a

--- TwitterIrcGatewayCore/AddIns/DLRIntegration/Samples/typablemap.rb
+++ TwitterIrcGatewayCore/AddIns/DLRIntegration/Samples/typablemap.rb
@@ -1,34 +1,34 @@
 module Misuzilla::IronRuby
-    module TypableMap
-        include Misuzilla::Applications::TwitterIrcGateway::AddIns::TypableMap
+  module TypableMap
+    include Misuzilla::Applications::TwitterIrcGateway::AddIns::TypableMap
 
-        @@commands = []
+    @@commands = []
 
-        def self.setup
-            @@typablemap_proc = Session.AddInManager.GetAddIn(Misuzilla::Applications::TwitterIrcGateway::AddIns::TypableMapSupport.to_clr_type).TypableMapCommands
+    def self.setup
+      @@typablemap_proc = Session.AddInManager.GetAddIn(Misuzilla::Applications::TwitterIrcGateway::AddIns::TypableMapSupport.to_clr_type).TypableMapCommands
 
-            # スクリプトアンロード時にコマンドを削除する
-            Session.AddInManager.GetAddIn(Misuzilla::Applications::TwitterIrcGateway::AddIns::DLRIntegration::DLRIntegrationAddIn.to_clr_type).BeforeUnload do |sender, e|
-                @@commands.each do |command|
-                    @@typablemap_proc.RemoveCommand(command)
-                end
-            end
+      # スクリプトアンロード時にコマンドを削除する
+      Session.AddInManager.GetAddIn(Misuzilla::Applications::TwitterIrcGateway::AddIns::DLRIntegration::DLRIntegrationAddIn.to_clr_type).BeforeUnload do |sender, e|
+        @@commands.each do |command|
+          @@typablemap_proc.RemoveCommand(command)
         end
-
-        def self.register(command, desc, &proc_cmd)
-            @@commands << command
-            @@typablemap_proc.AddCommand(command, desc, ProcessCommand.new{|p, msg, status, args|
-                proc_cmd.call(p, msg, status, args)
-            })
-        end
-
-        setup
+      end
     end
+
+    def self.register(command, desc, &proc_cmd)
+      @@commands << command
+      @@typablemap_proc.AddCommand(command, desc, ProcessCommand.new{|p, msg, status, args|
+        proc_cmd.call(p, msg, status, args)
+      })
+    end
+
+    setup
+  end
 end
 
 # TypableMap: test コマンドを追加する
 Misuzilla::IronRuby::TypableMap.register("test", "Test Command") do |p, msg, status, args|
-    System::Diagnostics::Trace.WriteLine("Test: #{status.to_string}")
+  System::Diagnostics::Trace.WriteLine("Test: #{status.to_string}")
 
-    true # true を返すとハンドルしたことになりステータス更新処理は行われない
+  true # true を返すとハンドルしたことになりステータス更新処理は行われない
 end
