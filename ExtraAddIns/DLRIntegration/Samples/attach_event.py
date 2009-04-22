912d5123d428d9505a76a71e55554e3b22d005a7
tomoyo <tomoyo@07c13e24-2e35-da11-a9e7-00b0d02dc07a>
2009-04-22 15:57:33 +0000

    内蔵したのでこっちは削除。
    git-svn-id: http://svn.misuzilla.org/etcetera/TwitterIrcGateway/trunk@539 07c13e24-2e35-da11-a9e7-00b0d02dc07a

--- ExtraAddIns/DLRIntegration/Samples/attach_event.py
+++
@@ -1,8 +0,0 @@
-import clr
-clr.AddReferenceByPartialName("System.Windows.Forms")
-from System.Windows.Forms import *
-
-def OnPreSendMessageTimelineStatus(sender, e):
-	e.Text = e.Text + " (by "+ e.Status.User.Name +")"
-
-Session.PreSendMessageTimelineStatus += OnPreSendMessageTimelineStatus
