6fda6974bb5c8559b5393f01e3c0f25c2c753fa2
tomoyo <tomoyo@07c13e24-2e35-da11-a9e7-00b0d02dc07a>
2009-04-23 15:17:40 +0000

    サンプルを更新。
    git-svn-id: http://svn.misuzilla.org/etcetera/TwitterIrcGateway/trunk@546 07c13e24-2e35-da11-a9e7-00b0d02dc07a

--- TwitterIrcGatewayCore/AddIns/DLRIntegration/Samples/console.py
+++ TwitterIrcGatewayCore/AddIns/DLRIntegration/Samples/console.py
@@ -6,8 +6,8 @@ import Misuzilla.Applications.TwitterIrcGateway
 import Misuzilla.Applications.TwitterIrcGateway.AddIns
 import Misuzilla.Applications.TwitterIrcGateway.AddIns.Console
 
-from Misuzilla.Applications.TwitterIrcGateway.AddIns.Console import Context
-from Misuzilla.Applications.TwitterIrcGateway.AddIns.DLRIntegration import DLRContext
+from Misuzilla.Applications.TwitterIrcGateway.AddIns.Console import ConsoleAddIn, Console, Context
+from Misuzilla.Applications.TwitterIrcGateway.AddIns.DLRIntegration import DLRIntegrationAddIn, DLRContext
 
 class TestContext(Context):
 	def Initialize(self):
@@ -16,8 +16,12 @@ class TestContext(Context):
 	def hauhau(self, args):
 		self.Console.NotifyMessage(("Hauhau: %s" % (args)))
 
+def onBeforeUnload(sender, e):
+	Session.AddInManager.GetAddIn[ConsoleAddIn]().UnregisterContext(DLRContext[TestContext].GetProxyType("DLRTest", TestContext))
+	console.Detatch()
+
 console = Misuzilla.Applications.TwitterIrcGateway.AddIns.Console.Console()
 console.Attach("#TestContext", Server, Session, DLRContext[TestContext].GetProxyType("Test", TestContext))
 
-def OnBeforeUnload(sender, e):
-	console.Detatch()
+Session.AddInManager.GetAddIn[DLRIntegrationAddIn]().BeforeUnload += onBeforeUnload
+Session.AddInManager.GetAddIn[ConsoleAddIn]().RegisterContext(DLRContext[TestContext].GetProxyType("DLRTest", TestContext), "DLRTest", "Context DLR implementation sample")
