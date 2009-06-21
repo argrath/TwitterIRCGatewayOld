1e93bab1ad0ca56caf0a7caefd3e19222e231d3d
tomoyo <tomoyo@07c13e24-2e35-da11-a9e7-00b0d02dc07a>
2009-06-21 16:55:38 +0000

    ちわちわ・わっちさんしてたりするとユーザのIDがとれないのでRemoveRedundantSuffixが効かない問題が発生していたのを修正。
    git-svn-id: http://svn.misuzilla.org/etcetera/TwitterIrcGateway/trunk@598 07c13e24-2e35-da11-a9e7-00b0d02dc07a

--- TwitterIrcGatewayCore/AddIns/RemoveRedundantSuffix.cs
+++ TwitterIrcGatewayCore/AddIns/RemoveRedundantSuffix.cs
@@ -9,7 +9,7 @@ namespace Misuzilla.Applications.TwitterIrcGateway.AddIns
     class RemoveRedundantSuffix : AddInBase
     {
         private static readonly Regex _suffixMatchRE = new Regex(@"^(\s*(\＜.{2,}\＞|\<.{2,}\>|\(.{2,}\)|\《.{2,}\》|\【.{2,}\】|\[.{2,}\]|\*.{2,}\*|lang:ja)+)$");
-        private Dictionary<Int32, LinkedList<String>> _lastStatusFromFriends;
+        private Dictionary<String, LinkedList<String>> _lastStatusFromFriends;
         
         public override void Initialize()
         {
@@ -17,14 +17,14 @@ namespace Misuzilla.Applications.TwitterIrcGateway.AddIns
             CurrentSession.ConfigChanged += new EventHandler<EventArgs>(Session_ConfigChanged);
 
             if (CurrentSession.Config.EnableRemoveRedundantSuffix)
-                _lastStatusFromFriends = new Dictionary<int, LinkedList<string>>();
+                _lastStatusFromFriends = new Dictionary<String, LinkedList<string>>();
         }
 
         void Session_ConfigChanged(object sender, EventArgs e)
         {
             if (_lastStatusFromFriends == null && CurrentSession.Config.EnableRemoveRedundantSuffix)
             {
-                _lastStatusFromFriends = new Dictionary<int, LinkedList<string>>();
+                _lastStatusFromFriends = new Dictionary<String, LinkedList<string>>();
             }
             else if (!CurrentSession.Config.EnableRemoveRedundantSuffix)
             {
@@ -37,11 +37,11 @@ namespace Misuzilla.Applications.TwitterIrcGateway.AddIns
             // Remove Redundant Suffixes
             if (CurrentSession.Config.EnableRemoveRedundantSuffix)
             {
-                if (!_lastStatusFromFriends.ContainsKey(e.Status.User.Id))
+                if (!_lastStatusFromFriends.ContainsKey(e.Status.User.ScreenName))
                 {
-                    _lastStatusFromFriends[e.Status.User.Id] = new LinkedList<string>();
+                    _lastStatusFromFriends[e.Status.User.ScreenName] = new LinkedList<string>();
                 }
-                LinkedList<String> lastStatusTextsByUId = _lastStatusFromFriends[e.Status.User.Id];
+                LinkedList<String> lastStatusTextsByUId = _lastStatusFromFriends[e.Status.User.ScreenName];
                 String suffix = DetectRedundantSuffix(e.Text, lastStatusTextsByUId);
                 lastStatusTextsByUId.AddLast(e.Text);
                 if (lastStatusTextsByUId.Count > 5)
