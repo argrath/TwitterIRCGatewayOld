021b0354f89aa2f37395a790cbecfff41a625560
tomoyo <tomoyo@07c13e24-2e35-da11-a9e7-00b0d02dc07a>
2009-10-26 11:33:40 +0000

    グループの新規作成と削除を追加。
    git-svn-id: http://svn.misuzilla.org/etcetera/TwitterIrcGateway/branches/hosting@673 07c13e24-2e35-da11-a9e7-00b0d02dc07a

--- TwitterIrcGatewayCore/AddIns/Console/Contexts/GroupContext.cs
+++ TwitterIrcGatewayCore/AddIns/Console/Contexts/GroupContext.cs
@@ -106,7 +106,41 @@ namespace Misuzilla.Applications.TwitterIrcGateway.AddIns.Console
 
             CurrentSession.SaveGroups();
         }
-            
+    
+        [Description("グループ(チャンネル)を新規作成します")]
+        public void New([Description("新しいグループ名")]String newChannelName)
+        {
+            if (!CheckNewChannelName(newChannelName))
+                return;
+
+            // 新チャンネルにJOIN
+            Group g = new Group(newChannelName);
+            CurrentSession.Groups.Add(newChannelName, g);
+            CurrentSession.JoinChannel(CurrentSession, g);
+            Console.NotifyMessage(String.Format("グループ名 {0} を作成しました。", newChannelName));
+
+            CurrentSession.SaveGroups();
+        }
+        
+        [Description("指定したグループ(チャンネル)を削除します")]
+        public void Delete([Description("削除するグループ名")]String oldChannelName)
+        {
+            if (!CurrentSession.Groups.ContainsKey(oldChannelName))
+            {
+                Console.NotifyMessage("エラー: 指定されたグループは見つかりませんでした。");
+                return;
+            }
+
+
+            // 旧チャンネルをPART
+            CurrentSession.SendServer(new PartMessage(oldChannelName, ""));
+            // 削除
+            CurrentSession.Groups.Remove(oldChannelName);
+            Console.NotifyMessage(String.Format("グループ名 {0} を削除しました。", oldChannelName));
+
+            CurrentSession.SaveGroups();
+        }
+        
         [Description("指定したグループ(チャンネル)の名前をコピーします")]
         public void Copy([Description("現在のグループ名")]String oldChannelName, [Description("新しいグループ名")]String newChannelName)
         {
@@ -124,6 +158,22 @@ namespace Misuzilla.Applications.TwitterIrcGateway.AddIns.Console
             CurrentSession.SaveGroups();
         }
     
+        private Boolean CheckNewChannelName(String newChannelName)
+        {
+            if (CurrentSession.Groups.ContainsKey(newChannelName) || String.Compare(CurrentSession.Config.ChannelName, newChannelName, true) == 0)
+            {
+                Console.NotifyMessage("エラー: 既に存在するグループ名を指定することは出来ません。");
+                return false;
+            }
+
+            if (!(newChannelName.StartsWith("#") && newChannelName.Length > 2))
+            {
+                Console.NotifyMessage("エラー: グループ(チャンネル)名は#で始まる必要があります。");
+                return false;
+            }
+
+            return true;
+        }
         private Boolean CheckChannelNamesForCopyOrRename(String oldChannelName, String newChannelName)
         {
             if (!CurrentSession.Groups.ContainsKey(oldChannelName))
@@ -137,25 +187,7 @@ namespace Misuzilla.Applications.TwitterIrcGateway.AddIns.Console
                 return false;
             }
 
-            if (String.Compare(oldChannelName, newChannelName, true) == 0)
-            {
-                Console.NotifyMessage("エラー: 新しいグループ名と古いグループ名を同じにすることはできません。");
-                return false;
-            }
-
-            if (CurrentSession.Groups.ContainsKey(newChannelName) || String.Compare(CurrentSession.Config.ChannelName, newChannelName, true) == 0)
-            {
-                Console.NotifyMessage("エラー: 既に存在するグループ名を指定することは出来ません。");
-                return false;
-            }
-
-            if (!(oldChannelName.StartsWith("#") && oldChannelName.Length > 2))
-            {
-                Console.NotifyMessage("エラー: グループ(チャンネル)名は#で始まる必要があります。");
-                return false;
-            }
-
-            return true;
+            return CheckNewChannelName(newChannelName);
         }
     }
 }
