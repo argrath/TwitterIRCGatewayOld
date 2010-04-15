e936b94f06e52ffc31a924948352b721a8d8dd6a
tomoyo <tomoyo@07c13e24-2e35-da11-a9e7-00b0d02dc07a>
2010-04-15 17:32:54 +0000

    UserStreamアドインを追加。
    git-svn-id: http://svn.misuzilla.org/etcetera/TwitterIrcGateway/branches/hosting@680 07c13e24-2e35-da11-a9e7-00b0d02dc07a

---
+++ ExtraAddIns/UserStream/UserStreamAddIn.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.ServiceModel.Dispatcher;
+using System.Text;
+using System.Threading;
+using Misuzilla.Applications.TwitterIrcGateway.AddIns.Console;
+
+namespace Misuzilla.Applications.TwitterIrcGateway.AddIns.UserStream
+{
+    public class UserStreamAddIn : AddInBase
+    {
+        private Thread _workerThread;
+        public override void Initialize()
+        {
+            Session.AddInsLoadCompleted += (sender, e) =>
+                                               {
+                                                   Session.AddInManager.GetAddIn<ConsoleAddIn>().RegisterContext<UserStreamContext>();
+                                                   Setup(Session.AddInManager.GetConfig<UserStreamConfig>().Enabled);
+                                               };
+        }
+        public override void Uninitialize()
+        {
+            Setup(false);
+        }
+    
+        internal void Setup(Boolean isStart)
+        {
+            if (_workerThread != null)
+            {
+                _workerThread.Abort();
+                _workerThread.Join();
+                _workerThread = null;
+            }
+
+            if (isStart)
+            {
+                _workerThread = new Thread(WorkerProcedure);
+                _workerThread.Start();
+            }
+        }
+    
+        private void WorkerProcedure()
+        {
+            FieldInfo fieldInfo = typeof (TwitterService).GetField("_credential", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetField);
+            CredentialCache credentials = fieldInfo.GetValue(CurrentSession.TwitterService) as CredentialCache;
+            
+            JsonQueryStringConverter converter = new JsonQueryStringConverter();
+            HttpWebRequest webRequest = WebRequest.Create("http://betastream.twitter.com/2b/user.json") as HttpWebRequest;
+            webRequest.Credentials = credentials.GetCredential(new Uri(CurrentSession.TwitterService.ServiceServerPrefix), "Basic");
+            webRequest.PreAuthenticate = true;
+            using (var response = webRequest.GetResponse())
+            {
+                StreamReader sr = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+                try
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        var line = sr.ReadLine();
+                        if (String.IsNullOrEmpty(line))
+                            continue;
+                        _Status statusJson = converter.ConvertStringToValue(line, typeof(_Status)) as _Status;
+                        if (statusJson == null || statusJson.id == 0 || statusJson.user.Protected)
+                            continue;
+
+                        Status status = new Status()
+                                            {
+                                                CreatedAt = statusJson.CreatedAt,
+                                                Text = statusJson.text,
+                                                Id = statusJson.id
+                                            };
+                        User user = new User()
+                                        {
+                                            Id = (Int32)statusJson.user.id,
+                                            Protected = statusJson.user.Protected,
+                                            ProfileImageUrl = statusJson.user.profile_image_url,
+                                            ScreenName = statusJson.user.screen_name
+                                        };
+                        status.User = user;
+                        Boolean friendCheckRequired = false;
+                        CurrentSession.TwitterService.ProcessStatus(status, (s) => CurrentSession.ProcessTimelineStatus(s, ref friendCheckRequired));
+                    }
+                }
+                catch (Exception e)
+                {
+                    CurrentSession.SendServerErrorMessage("エラー UserStream: " + e.Message);
+                }
+            }
+        }
+    }
+
+    [Description("User Stream設定コンテキストに切り替えます")]
+    public class UserStreamContext : Context
+    {
+        [Description("User Stream を有効にします")]
+        public void Enable()
+        {
+            var config = CurrentSession.AddInManager.GetConfig<UserStreamConfig>();
+            config.Enabled = true;
+            CurrentSession.AddInManager.SaveConfig(config);
+            CurrentSession.AddInManager.GetAddIn<UserStreamAddIn>().Setup(config.Enabled);
+            Console.NotifyMessage("User Stream を有効にしました。");
+        }
+        [Description("User Stream を無効にします")]
+        public void Disable()
+        {
+            var config = CurrentSession.AddInManager.GetConfig<UserStreamConfig>();
+            config.Enabled = false;
+            CurrentSession.AddInManager.SaveConfig(config);
+            CurrentSession.AddInManager.GetAddIn<UserStreamAddIn>().Setup(config.Enabled);
+            Console.NotifyMessage("User Stream を無効にしました。");
+        }
+    }
+    
+    public class UserStreamConfig : IConfiguration
+    {
+        public Boolean Enabled;
+    }
+
+    [DataContract]
+    class _Status
+    {
+        [DataMember]
+        public Int64 id { get; set; }
+        [DataMember]
+        public String text { get; set; }
+        [DataMember]
+        public String created_at { get; set; }
+        [DataMember]
+        public _User user { get; set; }
+
+        public DateTime CreatedAt { get { return DateTime.ParseExact(created_at, "ddd MMM dd HH:mm:ss zz00 yyyy", CultureInfo.InvariantCulture.DateTimeFormat); } }
+        //public Int64 in_reply_to { get; set; }
+    }
+
+    [DataContract]
+    class _User
+    {
+        [DataMember]
+        public Int64 id { get; set; }
+        [DataMember]
+        public String screen_name { get; set; }
+        [DataMember]
+        public String profile_image_url { get; set; }
+        [DataMember(Name = "protected")]
+        public Boolean Protected { get; set; }
+    }
+}
