ab0a4617f86485b313b4b7d933f7e31292369ead
SHIRAKATA Kentaro <argrath@ub32.org>
2013-01-24 00:32:59 +0900

    UTF8を使わない時でもRTを正しく表示

--- TwitterIrcGatewayCore/AddIns/InsertRetweetMark.cs
+++ TwitterIrcGatewayCore/AddIns/InsertRetweetMark.cs
@@ -10,13 +10,12 @@ namespace Misuzilla.Applications.TwitterIrcGateway.AddIns
         public override void Initialize()
         {
             Type encodingType = Server.Encoding.GetType();
-            if (encodingType == typeof(UTF8Encoding) || encodingType == typeof(UTF32Encoding) || encodingType == typeof(UnicodeEncoding))
             {
                 CurrentSession.PreProcessTimelineStatus += (sender, e) =>
                                                                      {
                                                                          if (e.Status.RetweetedStatus != null)
                                                                          {
-                                                                             e.Text = String.Format("♻ RT @{0}: {1}", e.Status.RetweetedStatus.User.ScreenName, e.Status.RetweetedStatus.Text);
+                                                                             e.Text = String.Format("RT @{0}: {1}", e.Status.RetweetedStatus.User.ScreenName, e.Status.RetweetedStatus.Text);
                                                                              e.Status.Entities = e.Status.RetweetedStatus.Entities; // 詰め替え
                                                                          }
                                                                      };
